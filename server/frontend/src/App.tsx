64ae2711883c712d3636a582abd01543469df5b8
Kris Jordan <kris@cs.unc.edu>
2023-12-22 17:39:26 -0500

    Pull in WS connection demo

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,22 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [webSocketOpen, setWebSocketOpen] = useState(false);
+  const [_webSocket, setWebSocket] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR!
-        </p>
-      </div>
-    </>
-  )
+  useEffect(() => {
+    let ws = new WebSocket("ws://localhost:8000/ws");
+    window.ws! = ws;
+    ws.onopen = () => {
+        setWebSocketOpen(true);
+        ws.send(JSON.stringify({ "type": "LS", "data": { "path": "/" } }));
+        ws.send(JSON.stringify({ "type": "RUN", "data": { "module": "hello" } }));
+        // ws.send(JSON.stringify({ "type": "RUN", "data": { "module": "error" } }));
+    };
+
+    ws.onmessage = (message) => {
+        setMessages((messages) => [...messages, message.data]);
+    };
+
+    ws.onclose = () => {
+        setWebSocketOpen(false);
+        setTimeout(() => {
+            setWebSocket(new WebSocket("ws://localhost:8000/ws"));
+        }, 10000);
+    };
+
+    setWebSocket(ws);
+
+    return () => {
+        ws.close()
+    };
+  }, []);
+
+
+    if (!webSocketOpen) {
+        return <div>Connecting...</div>;
+    } else {
+        let messagesHtml = messages.map((msg, index) => <p key={index}>${msg}</p>);
+        return <div>Connected! ${messagesHtml}</div>;
+    }
 }
 
 export default App
