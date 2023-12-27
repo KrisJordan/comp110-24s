fa31153b471b6d4af63d72cbefb139a7ebf49e8b
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2023-12-26 21:37:11 -0500

    Revert "WIP grouping"
    This reverts commit 421a1beb7c8e38e93b93d6667da43775404bc77d.

--- server/frontend/src/PyProcessUI.tsx
+++ server/frontend/src/PyProcessUI.tsx
@@ -1,21 +1,16 @@
 import React, { PropsWithChildren, useCallback, useEffect, useState } from "react";
 import { PyProcess, PyProcessState } from "./PyProcess";
 import useWebSocket from "./useWebSocket";
-import { useDebounce } from "@uidotdev/usehooks";
 import { parseJsonMessage } from "./Message";
 import { StdErrMessage } from "./StdErrMessage";
 
 interface PyProcessUIProps {
-    pyProcess: PyProcess,
-    groupingEnabled: boolean,
-    minGroupSize: number,
-    msgGroupTimeSeparationInMS: number,
+    pyProcess: PyProcess
 }
 
 type StdOut = {
     type: 'stdout';
     line: string;
-    timestamp: number;
 }
 
 type StdErr = {
@@ -29,23 +24,13 @@ type StdIn = {
     response?: string;
 }
 
-type StdOutGroup = {
-    type: 'group';
-    children: (StdOut & Timestamped)[];
-}
-
-type Timestamped = {
-    timestamp: number;
-}
-
-type StdIO = (StdOut | StdErr | StdIn | StdOutGroup) & Timestamped;
+type StdIO = StdOut | StdErr | StdIn;
 
 
 export function PyProcessUI(props: PropsWithChildren<PyProcessUIProps>) {
     const { lastMessage, readyState, sendJsonMessage } = useWebSocket();
     const [pyProcess, setPyProcess] = useState(props.pyProcess);
     const [stdio, setStdIO] = useState<StdIO[]>([]);
-    const debouncedStdIO = useDebounce(stdio, 500);
     const [stdinValue, setStdinValue] = useState<string>("");
 
     useEffect(() => {
@@ -62,16 +47,15 @@ export function PyProcessUI(props: PropsWithChildren<PyProcessUIProps>) {
                     }
                     break;
                 case 'STDOUT':
-                    let time = Date.now();
                     if (!message.data.is_input_prompt) {
-                        setStdIO((prev) => prev.concat({ type: 'stdout', line: message?.data.data, timestamp: time }))
+                        setStdIO((prev) => prev.concat({ type: 'stdout', line: message?.data.data }))
                     } else {
-                        setStdIO((prev) => prev.concat({ type: 'stdin', prompt: message?.data.data, timestamp: time }))
+                        setStdIO((prev) => prev.concat({ type: 'stdin', prompt: message?.data.data }))
                     }
                     break;
                 case 'STDERR':
                     if (!message.data.is_input_prompt) {
-                        setStdIO((prev) => prev.concat({ type: 'stderr', line: message?.data.data, timestamp: Date.now() }))
+                        setStdIO((prev) => prev.concat({ type: 'stderr', line: message?.data.data }))
                     }
                     break;
                 case 'EXIT':
@@ -134,64 +118,6 @@ export function PyProcessUI(props: PropsWithChildren<PyProcessUIProps>) {
         });
     }, [stdinValue]);
 
-    useEffect(() => {
-        if (!props.groupingEnabled) return;
-
-        let group = [];
-        let existingGroup: StdIO | null = null;
-        for (let i = debouncedStdIO.length - 1; i > 0; i--) {
-            let line = debouncedStdIO[i];
-            let prevLine = debouncedStdIO[i - 1];
-
-            let exitLoop = false;
-            switch (line.type) {
-                case 'stdout':
-                    if (line.timestamp - prevLine.timestamp > props.msgGroupTimeSeparationInMS) {
-                        exitLoop = true;
-                        break;
-                    }
-                    group.unshift(line);
-                    break;
-                case 'group':
-                    exitLoop = true;
-                    if (line.timestamp - prevLine.timestamp > props.msgGroupTimeSeparationInMS) {
-                        break;
-                    }
-                    existingGroup = line;
-                    break;
-                default:
-                    exitLoop = true;
-                    break;
-            }
-            if (exitLoop) break;
-        }
-
-        if (group.length === 0) {
-            return;
-        }
-
-        if (existingGroup !== null) {
-            existingGroup.children.push(...group);
-            existingGroup.timestamp = group.pop()?.timestamp as number;
-
-            // setStdIO((oldStdio) => {
-            //     if (oldStdio[oldStdio.length - 1].timestamp < ) {
-            //         return debouncedStdIO.slice(0, 0 - group.length);
-            //     } else {
-            //         return oldStdio;
-            //     }
-            // });
-        } else if (group.length >= props.minGroupSize) {
-            setStdIO([...debouncedStdIO.slice(0, 0 - group.length),
-            {
-                type: 'group',
-                children: [...group],
-                timestamp: group.pop()?.timestamp as number
-            }
-            ]);
-        }
-    }, [debouncedStdIO]);
-
     return <div className="prose">
         <h3>{status}</h3>
         {stdio.map((line, idx) => {
@@ -211,22 +137,6 @@ export function PyProcessUI(props: PropsWithChildren<PyProcessUIProps>) {
                     return <p key={idx}>{line.line}</p>
                 case 'stderr':
                     return <StdErrMessage key={idx} line={line.line} />;
-                case 'group':
-                    return <div key={idx}>
-                        <p>{line.children[0].line}</p>
-                        <details>
-                            <summary>
-                                [{line.children.length - 1}] more lines collapsed
-                            </summary>
-                            <div>
-                                {
-                                    line.children.slice(1).map((line, subIdx) => {
-                                        return <p key={`${idx}-${subIdx}`}>{line.line}</p>
-                                    })
-                                }
-                            </div>
-                        </details>
-                    </div>
             }
         })}
     </div>;
