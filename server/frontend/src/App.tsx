6d3c721e46ce914e9fe051b505de595810fbb7a0
Kris Jordan <krisjordan@gmail.com>
2023-12-27 09:01:24 -0500

    Implement initial routing (#8)
    Co-authored-by: noahsmiths <90880131+noahsmiths@users.noreply.github.com>

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -4,7 +4,7 @@ import NamespaceTree from './NamespaceTree';
 import { Message } from './Message';
 import { Module } from './NamespaceTree';
 import { PyProcess, PyProcessState } from './PyProcess';
-import { PyProcessUI } from './PyProcessUI';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import './App.css';
 
@@ -13,7 +13,8 @@ function App() {
     const [runningProcess, setRunningProcess] = useState<PyProcess | null>(null);
     const [requestId, setRequestId] = useState<number>(0);
     const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket();
-    
+    const navigate = useNavigate();
+
     useEffect(() => {
         if (lastJsonMessage && Object.keys(lastJsonMessage).length !== 0) {
             let message = lastJsonMessage as Message;
@@ -29,36 +30,39 @@ function App() {
         [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
     }[readyState];
 
-    const runModule = useCallback((module: Module) => {
-        let moduleStr = module.full_path.replace(/^\.\//, '').replace(/\//g, '.').replace('.py', '');
-        sendJsonMessage({"type": "RUN", "data": {"module": moduleStr, "request_id": requestId}});
+    const runModule = useCallback((moduleStr: string) => {
+        sendJsonMessage({ "type": "RUN", "data": { "module": moduleStr, "request_id": requestId } });
         setRequestId(prev => prev + 1);
-        setRunningProcess({ 
+        setRunningProcess({
             module: moduleStr,
             requestId: requestId,
             state: PyProcessState.STARTING
         });
     }, [requestId]);
 
-    let selectedUI = runningProcess ? <PyProcessUI pyProcess={runningProcess} key={runningProcess.requestId} /> : null;
+    function routeToModuleRunner(module: Module) {
+        let moduleStr = module.full_path.replace(/^\.\//, '').replace(/\//g, '.').replace('.py', '');
+        setRunningProcess(null);
+        navigate(`/run/${moduleStr}`);
+    }
 
     return <>
-            <div className="navbar bg-neutral text-neutral-content rounded-box">
-                <div className="text-xl flex-1 ml-2">{runningProcess ? runningProcess.module : 'Select a Module'}</div>
+        <div className="navbar bg-neutral text-neutral-content rounded-box">
+            <div className="text-xl flex-1 ml-2">{runningProcess ? runningProcess.module : 'Select a Module'}</div>
+        </div>
+        <div className="flex">
+            <div className="flex-none">
+                <NamespaceTree selectModule={routeToModuleRunner} />
             </div>
-            <div className="flex">
-                <div className="flex-none">
-                    <NamespaceTree selectModule={runModule}/>
-                </div>
-                <div className="flex-1 ml-4 mt-4">
-                    { selectedUI }
-                </div>
+            <div className="flex-1 ml-4 mt-4">
+                <Outlet context={{ runningProcess, runModule }} />
             </div>
-            <ul className="hidden">
-                {messageHistory.map((message, idx) => (
-                    <li key={idx}>{message}</li>
-                ))}
-            </ul>
+        </div>
+        <ul className="hidden">
+            {messageHistory.map((message, idx) => (
+                <li key={idx}>{message}</li>
+            ))}
+        </ul>
     </>;
 }
 
