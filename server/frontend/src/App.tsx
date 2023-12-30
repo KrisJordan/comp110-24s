23ae25f687c1b7d94acf8aa437fd54b3d0db5c11
noahsmiths <90880131+noahsmiths@users.noreply.github.com>
2023-12-30 16:10:19 -0500

    Make file view collapsable in App.tsx

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
     const [messageHistory, setMessageHistory] = useState<string[]>([]);
     const [runningProcess, setRunningProcess] = useState<PyProcess | null>(null);
     const [requestId, setRequestId] = useState<number>(0);
+    const [showFiles, setShowFiles] = useState<boolean>(true);
     const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket();
     const navigate = useNavigate();
 
@@ -48,10 +49,19 @@ function App() {
 
     return <>
         <div className="navbar bg-neutral text-neutral-content rounded-box">
+            <button onClick={() => { setShowFiles(prev => !prev) }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current mx-3">
+                    {
+                        showFiles
+                            ? <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001"></path>
+                            : <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    }
+                </svg>
+            </button>
             <div className="text-xl flex-1 ml-2">{runningProcess ? runningProcess.module : 'Select a Module'}</div>
         </div>
         <div className="flex">
-            <div className="flex-none">
+            <div className={`flex-none ${!showFiles && 'hidden'}`}>
                 <NamespaceTree selectModule={routeToModuleRunner} />
             </div>
             <div className="flex-1 ml-4 mt-4">
