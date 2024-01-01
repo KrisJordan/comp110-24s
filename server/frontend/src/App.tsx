37d736756a6854cbe90750779d47493a0d1f17ce
Noah Smith <nsmith2@unc.edu>
2024-01-01 01:10:32 -0500

    Add a toggle to make the the file tree (NamespaceTree) collapsable within App.tsx (#14)

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Message } from './Message';
 import { Module } from './NamespaceTree';
 import { PyProcess, PyProcessState } from './PyProcess';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
     const [messageHistory, setMessageHistory] = useState<string[]>([]);
     const [runningProcess, setRunningProcess] = useState<PyProcess | null>(null);
     const [requestId, setRequestId] = useState<number>(0);
+    const [showFiles, setShowFiles] = useState<boolean>(true);
     const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket();
     const navigate = useNavigate();
 
@@ -48,10 +50,13 @@ function App() {
 
     return <>
         <div className="navbar bg-neutral text-neutral-content rounded-box">
+            <button onClick={() => { setShowFiles(prev => !prev) }}>
+                <Icon className="mx-3" icon={showFiles ? "ph:x" : "ci:hamburger-md"} height={25} />
+            </button>
             <div className="text-xl flex-1 ml-2">{runningProcess ? runningProcess.module : 'Select a Module'}</div>
         </div>
         <div className="flex">
-            <div className="flex-none">
+            <div className={`flex-none ${!showFiles && 'hidden'}`}>
                 <NamespaceTree selectModule={routeToModuleRunner} />
             </div>
             <div className="flex-1 ml-4 mt-4">
